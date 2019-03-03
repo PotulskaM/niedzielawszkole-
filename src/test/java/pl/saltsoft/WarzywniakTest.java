0734f31589b5d4e9428f74c91a73caf28f469200
Małgorzata Potulska <m.pootulska@gmail.com>
2019-03-03 15:29:21 +0100

    - testy z błędami

---
+++ src/test/java/pl/saltsoft/WarzywniakTest.java
@@ -0,0 +1,16 @@
+package pl.saltsoft;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WarzywniakTest {
+
+    @Test
+    public void czyKupicJablka() {
+        Warzywniak warzywniakNaUlicyMilej = new Warzywniak();
+        assertEquals();assertEquals(warzywniakNaUlicyMilej.czyKupicJablka(18), "nie");
+
+
+    }
+}
